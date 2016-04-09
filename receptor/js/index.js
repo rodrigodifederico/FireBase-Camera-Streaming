e566758ec431211897eb3e68e85bc9597309efdb
Rodrigo <rodrigovitachi@users.noreply.github.com>
2016-04-09 12:33:33 -0300

    Update index.js

--- receptor/js/index.js
+++ receptor/js/index.js
@@ -1,6 +1,6 @@
 
 $( document ).ready(function() {
-    root = new Firebase('https://1oteste.firebaseio.com/mensagem');
+    root = new Firebase('https://myapp.firebaseio.com/node');
 
 	root.on('value', function(snapshot){
 		$("#imagem").attr('src', snapshot.val())
